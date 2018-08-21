5d4808911d289410beed8c351e8490856dc2f8cc
Gel <gel@leandro@alertaapp.com.br>
2018-08-21 00:05:59 -0300

    trying to add flex and some style to main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,20 @@
 import React from 'react'
 
+import './Main.css'
 import NurseList from '../components/NurseList'
 import Calendar from '../components/Calendar'
 import Header from '../components/Header'
 
 const Main = () => {
-  return(<div>
-    <Header />
-    <NurseList />
-    <Calendar />
-  </div>)
+  return(<div className='container' style={ { display: 'flex', flexDirection: 'column' } } >
+    <div style={ { flex: 1 } } >
+      <Header />
+    </div>
+    <div style={ { flex: 1 } } >
+      <NurseList />
+      <Calendar />
+    </div>
+</div>)
 }
 
 export default Main
